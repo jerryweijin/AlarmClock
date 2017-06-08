d0b424aa184b80e4eb54c606f0e722850515ac2f
Jerry <jerryweijin@hotmail.com>
2017-06-07 19:01:39 -0700

    Initial commit

---
+++ app/src/main/java/com/jerryweijin/alarmclock/AboutClockActivity.java
@@ -0,0 +1,20 @@
+package com.jerryweijin.alarmclock;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jerry on 6/7/2017.
+ */
+
+public class AboutClockActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_clock);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.about_clock_in_cap);
+    }
+}
---
+++ app/src/main/java/com/jerryweijin/alarmclock/MainActivity.java
@@ -0,0 +1,44 @@
+package com.jerryweijin.alarmclock;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+        actionBar.setElevation(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_delete:
+                break;
+            case R.id.menu_about_clock:
+                Intent intent = new Intent(this, AboutClockActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+}
